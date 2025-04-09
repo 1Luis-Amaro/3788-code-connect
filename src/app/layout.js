7819078c559cf7543c80173bf826c58e0c844c6f
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-04-09 09:21:13 -0300

    Configuração do React QUery / 01 instalado e configurando

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Prompt } from "next/font/google";
 import { Aside } from "@/components/Aside";
 import "./globals.css";
 import { SearchForm } from "@/components/SearchForm";
+import { ReactQueryProvider } from "./ReactQueryProvider";
+
 
 export const metadata = {
   title: "Code Connect",
@@ -17,6 +19,7 @@ const prompt = Prompt({
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" className={prompt.className}>
+      <ReactQueryProvider>
       <body>
         <div className="app-container">
           <div>
@@ -28,6 +31,8 @@ export default function RootLayout({ children }) {
           </div>
         </div>
       </body>
+      </ReactQueryProvider>
+      
     </html>
   );
 }
