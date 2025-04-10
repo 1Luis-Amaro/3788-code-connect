414772dd91cf4541aa0d1fc67fa43584cf136970
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-04-10 14:50:03 -0300

    Dominando o cache e estrategias de Fetch / 04 - Queries paralelas com useQueries

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,59 @@
 "use client";
 
-import {useQuery} from "@tanstack/react-query"
+import { useQuery, useQueries } from "@tanstack/react-query";
 import { CardPost } from "@/components/CardPost";
 import { Spinner } from "@/components/Spinner";
 import styles from "./page.module.css";
 import Link from "next/link";
 
-const fetchPosts = async ({page}) => {
-  const results = await fetch(`http://localhost:3000/api/posts?page=${page}`)
+const fetchPosts = async ({ page }) => {
+  const results = await fetch(`http://localhost:3000/api/posts?page=${page}`);
 
-  const data = await results.json()
+  const data = await results.json();
 
-  return data
-}
+  return data;
+};
+
+export const fetchPostsRating = async ({ postId }) => {
+  const results = await fetch(
+    `http://localhost:3000/api/post?postId=${postId}`
+  );
+
+  const data = await results.json();
+
+  return data;
+};
+
+
 
 export default function Home({ searchParams }) {
   const currentPage = parseInt(searchParams?.page || 1);
   const searchTerm = searchParams?.q;
 
-  const {data: posts, isLoading, isFetching} = useQuery({
+  const {
+    data: posts,
+    isLoading,
+    isFetching,
+  } = useQuery({
     queryKey: ["posts", currentPage], //definir a key, com isso ele genrencia o cash
-    queryFn: () => fetchPosts({page: currentPage}), //lugar que o useQuery vai buscar os dados
-    staleTime: 2000,//esse dado vai demorar 15 segundos pra se tornar obsoleto, quando eu trocar de pagina ele vai usar do cash e não de uma requisição
-    refetchOnWindowFocus: false,
-    //gcTime:2000, aqui ja coloco um tempo para ele excluir o dado
+    queryFn: () => fetchPosts({ page: currentPage }), //lugar que o useQuery vai buscar os dados
+    staleTime: 2000, // O dado será considerado "fresco" por 2 segundos. Após isso, ele pode ser refetchado.
+    //refetchOnWindowFocus: false, // Desativa a refetch automática ao voltar o foco para a aba do navegador.
+    //gcTime: 2000, // O dado será removido do cache após 2 segundos desde que não esteja mais sendo usado
+  });
+
+  const postRatingQueries = useQueries({
+    queries: posts?.data.length > 0 ? posts.data.map((post) => ({
+      queryKey: ["postHome", post.id],
+      queryFn: () => fetchPostsRating({postId: post.id}),
+      enabled: !!post.id,
+    }))
+    : [],
   })
 
-  const ratingsAndCartegoriesMap = null;
+  console.log("postRatingQueries", postRatingQueries)
+ 
+  const ratingsAndCartegoriesMap = [] 
 
   return (
     <main className={styles.grid}>
